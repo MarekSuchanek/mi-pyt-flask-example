1394d143fcd2679ec5bd450f56b772ad010cb3c9
Marek Suchánek <suchama4@fit.cvut.cz>
2017-10-11 13:34:12 +0200

    Hosted at PythonAnywhere

--- greet.py
+++ greet.py
@@ -15,7 +15,7 @@ class GreeterApp(Flask):
 
     def greet(self):
         self.greetings += 1
-        return 'Hello! Welcome to my public page!'
+        return 'Hello! Welcome to my public page @ PythonAnywhere!'
 
 
 app = GreeterApp(__name__)
